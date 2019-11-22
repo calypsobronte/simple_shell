987ead8da9f5ebeee6578715611e781f84c95994
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-11-22 00:40:39 +0000

    add  librerie for shell

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef simple_shell
+#define simple_shell
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#endif /* libraries Simple Shell Holberton */
