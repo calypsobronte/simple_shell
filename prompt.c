8418cb87ee0da690ec2e2492b1bca13ae770ea0b
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-11-27 20:05:24 +0000

    Optimitation of function main

--- prompt.c
+++ prompt.c
@@ -7,12 +7,9 @@
  */
 int main(int ac, char **av)
 {
-
-	char *buffer = NULL, *TokenTemporal = NULL;
-	char **TokenMain = NULL;
+	char *buffer = NULL, *TokenTemporal = NULL, **TokenMain = NULL;
 	size_t BUFFSIZE = 32, characters = 0;
 	int i = 0, j = 0;
-
 	while (1)
 	{
 		buffer = malloc(BUFFSIZE * sizeof(char));
@@ -21,14 +18,11 @@ int main(int ac, char **av)
 			perror("Unable to allocate buffer");
 			exit(1);
 		}
-		printf("HolbiPro");
-		printf("$ ");
+		printf("HolbiPro $ ");
 		characters = getline(&buffer, &BUFFSIZE, stdin);
 		TokenMain = malloc(sizeof(char *) * BUFFSIZE);
 		if (TokenMain == NULL)
-		{
 			return (0);
-		}
 		if (characters == (size_t)-1)
 		{
 			free(TokenMain);
@@ -46,199 +40,10 @@ int main(int ac, char **av)
 			i++;
 		}
 		get_func(TokenMain[0], TokenMain);
-		j = 0;
 		while (TokenMain[j] != NULL)
 			j++;
 		free_shell(TokenMain, j);
 		free(buffer);
 		}
 		return (0);
-}
-
-/**
- * free_shell - funcion de para liberar en el main
- * @TokenMain: char
- * @size: size del argumento
- */
-void free_shell(char **TokenMain, int size)
-{
-int i;
-for (i = 0; i < size; i++)
-{
-if (TokenMain[i] != NULL)
-	free(TokenMain[i]);
-}
-free(TokenMain);
-}
-
-/**
- * get_func - funcion con sus principales funcionamientos con los diferentes comando y tags
- * @TokenMain: char
- * @Token: token
- * Return: 1
- */
-int get_func(char *TokenMain, char **Token)
-{
-
-	char *search = NULL;
-	pid_t child_pid = 0;
-	int status = 0;
-	int i = 0;
-
-	while (Token[i] != NULL)
-		i++;
-	Token[i] = NULL;
-	if (i == 1)
-	{
-		TokenMain[_strlen(TokenMain) - 1] = '\0';
-	}
-	else if (i > 1)
-	{
-		Token[i - 1][_strlen(Token[i - 1]) - 1] = '\0';
-	}
-	/*
-    * cambio 27 nov
-    */
-	if (TokenMain[0] == '/' || TokenMain[0] == '.')
-	{
-		search = malloc(sizeof(char) * _strlen(TokenMain) + 1);
-		_strcpy(search, TokenMain);
-
-	}
-	else
-	{
-		search = malloc(sizeof(char) * _strlen(TokenMain) + 6);
-		_strcpy(search, "/bin/");
-		_strcat(search, TokenMain);
-	}
-
-	free(Token[0]);
-	Token[0] = malloc(sizeof(char) * _strlen(search) + 1);
-	_strcat(Token[0], search);
-
-
-	if (access(search, X_OK | F_OK) == 0)
-	{
-		child_pid = fork();
-		if (child_pid == -1)
-		{
-			perror("Error:");
-			return (1);
-		}
-		if (child_pid == 0)
-		{
-			execve(search, Token, NULL);
-			return (0);
-		}
-		else
-			/*
-            * waitpid(child_pid, &status, 0);
-            */
-			waitpid(child_pid, NULL, 0);
-
-	}
-	else
-		perror("./shell");
-	free(search);
-	return (1);
-}
-
-/**
- * _strcat - function that concatena two strings
- * @dest: leeter
- * @src: letter
- * Return: dest of string
- */
-char *_strcat(char *dest, char *src)
-{
-int words = 0, b;
-while (dest[words] != 0)
-words++;
-for (b = 0; src[b] != 0; b++, words++)
-dest[words] = src[b];
-dest[words] = 0;
-return (dest);
-}
-
-/**
- * _strcpy - copy of string
- * @dest: string
- * @src: string
- * Return: dest a copy
- */
-char *_strcpy(char *dest, char *src)
-{
-int cont_words;
-for (cont_words = 0; src[cont_words] != 0; cont_words++)
-dest[cont_words] = src[cont_words];
-dest[cont_words] = 0;
-return (dest);
-}
-
-/**
- * _strlen - counts length of a string
- * @str: - letter string
- * Return: number of string
- */
-int _strlen(char *str)
-{
-char *point = str;
-size_t _string;
-
-_string = 0;
-if (str == NULL)
-	return (_string);
-while (*point++ != '\0')
-	_string++;
-return (_string);
-}
-
-/**
-  * _strtok - tokenizes a string
-  * @str: string to tokenize
-  * @delimitador: string that contains the delimitadoriters
-  * Return: pointer to position in string of a null terminated word.
-  */
-char *_strtok(char *str, const char *delimitador)
-{
-	static char *run_string, *end_string, *end;
-	unsigned int i, j;
-	int words = 0;
-
-	if (str == NULL)
-	{
-		if (run_string == NULL || end == NULL)
-			return (NULL);
-		str = end + 1;
-	}
-	else
-		end_string = str + _strlen(str);
-	for (i = 0; str + i < end_string; i++)
-	{
-		//lina
-		for (j = 0; delimitador != NULL && delimitador[j] != '\0'; j++)
-		{
-			if (str[i] == delimitador[j])
-			{
-				if (words == 1)
-				{
-					str[i] = '\0';
-					end = str + i;
-					return (run_string);
-				}
-				break;
-			}
-		}
-
-		if (delimitador[j] == '\0' && words == 0)
-		{
-			words = 1;
-			run_string = str + i;
-		}
-	}
-	end = NULL;
-
-	if (words == 1)
-		return (run_string);
-	return (NULL);
 }
