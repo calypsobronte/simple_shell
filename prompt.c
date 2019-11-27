3f3e82c5322a3c708e20fe5a8e7fe6d18f5d1ad1
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-11-27 16:06:19 +0000

    added of function strcat

--- prompt.c
+++ prompt.c
@@ -139,3 +139,21 @@ int get_func(char *TokenMain, char **Token)
 	free(search);
 	return (1);
 }
+
+/**
+ * _strcat - function that concatena two strings
+ * @dest: leeter
+ * @src: letter
+ * Return: dest of string
+ */
+char *_strcat(char *dest, char *src)
+{
+int words = 0, b;
+while (dest[words] != 0)
+words++;
+for (b = 0; src[b] != 0; b++, words++)
+dest[words] = src[b];
+dest[words] = 0;
+return (dest);
+}
+
