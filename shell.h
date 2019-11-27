a00b7207fed977e6a3425be4ec916e513136a042
Christian David Suárez Guerra <darkdrakkar@gmail.com>
2019-11-27 15:06:36 -0500

    all function prototypes filled

--- shell.h
+++ shell.h
@@ -2,21 +2,22 @@
 #define simple_shell
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
-#include <sys/stat.h>
 #include <limits.h>
-#include <sys/types.h>
 #include <signal.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
 int main(int ac, char **av);
 void free_shell(char **TokenMain, int size);
-int get_func(char * TokenMain, char **Token);
+int get_func(char *TokenMain, char **Token);
 
-int _strlen(char *str);
-char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *str);
 char *_strtok(char *str, const char *delimitador);
+int exe_fun(char **Token, char *search);
 #endif /* libraries Simple Shell Holberton */
