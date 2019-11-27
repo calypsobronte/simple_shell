62bca2da34513197222fd9cb79b3b1a87a668ec3
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-11-27 17:36:28 +0000

    update libraries in shell

--- shell.h
+++ shell.h
@@ -2,21 +2,21 @@
 #define simple_shell
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
-#include <limits.h>
-#include <signal.h>
 #include <sys/stat.h>
+#include <limits.h>
 #include <sys/types.h>
+#include <signal.h>
 #include <sys/wait.h>
 
-int main(int ac, char **av);
+int main(int ac, char **av)
 void free_shell(char **TokenMain, int size);
-int get_func(char *TokenMain, char **Token);
+int get_func(char * TokenMain, char **Token);
 
-char *_strcat(char *dest, char *src);
-char *_strcpy(char *dest, char *src);
 int _strlen(char *str);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
 char *_strtok(char *str, const char *delimitador);
 #endif /* libraries Simple Shell Holberton */
